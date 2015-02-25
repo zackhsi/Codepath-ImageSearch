59dbea8bd2e93b57d8bd42537ab530688958bf59
Zack Hsi <zhsi@lyft.com>
2015-02-24 23:26:51 -0800

    Add dummy click handler to button

--- app/src/main/java/com/zackhsi/imagesearch/SearchActivity.java
+++ app/src/main/java/com/zackhsi/imagesearch/SearchActivity.java
@@ -4,16 +4,41 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.GridView;
+import android.widget.Toast;
 
 
 public class SearchActivity extends ActionBarActivity {
+    EditText etQuery;
+    Button btnSearch;
+    GridView gvResults;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        findViews();
+        setupHandlers();
     }
 
+    private void findViews() {
+        etQuery = (EditText) findViewById(R.id.etQuery);
+        btnSearch = (Button) findViewById(R.id.btnSearch);
+        gvResults = (GridView) findViewById(R.id.gvResults);
+    }
+
+    private void setupHandlers() {
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getBaseContext(), "Search query: " + etQuery.getText().toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
